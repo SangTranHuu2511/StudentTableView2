1803ed30fdc9f4964effa9778015d04897176319
locxoay03 <sangtranhuu1996@gmail.com>
2017-09-19 09:26:14 +0700

    Add Student

--- StudentTableView/DetailViewController.swift
+++ StudentTableView/DetailViewController.swift
@@ -1,35 +1,33 @@
 //
 //  DetailViewController.swift
-//  StudentTableView
+//  DemoTableView
 //
-//  Created by SangTran on 9/19/17.
+//  Created by SangTran on 9/18/17.
 //  Copyright © 2017 Sang Tran. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var tempTitle: String?
+    var delegate: DetailViewControllerDelegate?
+    
+    @IBOutlet weak var studentTextFeid: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        studentTextFeid.text = tempTitle
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveStudent(_ sender: UIButton) {
+        delegate?.passData(name: studentTextFeid.text!)
+        navigationController?.popViewController(animated: true)
+        
+        
     }
-    */
+}
 
+protocol DetailViewControllerDelegate {
+    func passData(name: String)
+    
 }
